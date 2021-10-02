13875a3c66f1aa9d3bb5ff089f45987ec4d38969
IanStratico <straticoian@gmail.com>
2021-10-01 23:25:51 -0300

    comienzo conexiones falta probar

--- Memoria/include/main.h
+++ Memoria/include/main.h
@@ -2,6 +2,7 @@
 #define MAIN_MEMORIA_H_
 
 #include "config.h"
-
+#include "conexiones.h"
+#include "../../Shared/include/sockets.h"
 t_config_memoria* MEMORIA_CFG;
 #endif
--- Memoria/main.c
+++ Memoria/main.c
@@ -13,10 +13,22 @@ int main(){
 	MEMORIA_CFG = initialize_cfg();
 	main_log = log_create("memoria.log","memoria",true,LOG_LEVEL_INFO);
 
+	int memoria_server;
+
 	if(!cargar_configuracion(MEMORIA_CFG)){
 		cerrar_programa(main_log,MEMORIA_CFG);
 		return EXIT_FAILURE;
 	}
+
+	memoria_server = iniciar_servidor(main_log,SERVERNAME,MEMORIA_CFG->IP,MEMORIA_CFG->PUERTO);
+	if(!memoria_server){
+		cerrar_programa(main_log,MEMORIA_CFG);
+		return EXIT_FAILURE;
+	}
+
+	while(server_escuchar(SERVERNAME,memoria_server));
+
+	liberar_conexion(&memoria_server);
 
 	printf("Hola mundo");
 	cerrar_programa(main_log,MEMORIA_CFG);
