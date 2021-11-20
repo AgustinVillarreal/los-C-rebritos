fdcbe16cf96a4f203afc88feb9c1be59fe35861a
LucasRebuffo <lucasrebuloco@gmailcom>
2021-11-20 16:50:16 -0300

    Commit para mantenet lo que tenia

--- SWAmP/main.c
+++ SWAmP/main.c
@@ -4,11 +4,35 @@ extern t_config_swamp* cfg;
 extern t_log* logger;
 
 extern t_list* areas_de_swap;
+extern t_list* tablas_de_frames_swap;
 
 extern sem_t SEM_COMPACTACION_DONE;
 extern sem_t SEM_COMPACTACION_START;
 
 int swamp_server;
+
+/* typedef struct {
+    uint32_t pid;
+    uint32_t nro_pagina;
+    uint32_t inicio;
+    uint32_t nro_swap;
+    bool libre;
+} frame_swap_t; */
+
+void mostrar_tabla_swap(t_list* tabla){
+
+  for(int i = 0 ; i < list_size(tabla) ; i++){
+    frame_swap_t* frame = list_get(tabla,i);
+    log_info(logger,"PID: %d",frame->pid);
+    log_info(logger,"NRO PAGINA: %d",frame->nro_pagina);
+    log_info(logger,"INICIO: %d",frame->inicio);
+    log_info(logger,"NRO SWAP: %d",frame->nro_swap);
+    log_info(logger,"LIBRE: %d",frame->libre);
+    log_info(logger,"------------------------");
+
+  }
+
+}
 
 int main(){
 
@@ -25,6 +49,35 @@ int main(){
 
 
   	//while(server_escuchar(SERVERNAME, swamp_server));
+
+    char* data1 = "1111111111111111111111111111111111111111111111111111111111111111";
+    char* data2 = "2222222222222222222222222222222222222222222222222222222222222222";
+    char* data3 = "3333333333333333333333333333333333333333333333333333333333333333";
+    proceder_asignacion_fija(0,1,1,(void*)data1);
+    proceder_asignacion_fija(0,2,1,(void*)data2);
+    proceder_asignacion_fija(0,3,1,(void*)data3);
+    proceder_asignacion_fija(0,1,2,(void*)data1);
+    proceder_asignacion_fija(0,1,4,(void*)data1);
+    proceder_asignacion_fija(0,1,9,(void*)data1);
+    proceder_asignacion_fija(0,1,10,(void*)data1);
+    proceder_asignacion_fija(0,1,7,(void*)data1);
+    proceder_asignacion_fija(0,1,8,(void*)data1);
+    proceder_asignacion_fija(0,1,5,(void*)data1);
+    proceder_asignacion_fija(0,1,3,(void*)data1);
+    /* proceder_asignacion_fija(0,1,6,(void*)data); */
+ 
+
+    
+  /*   proceder_asignacion_fija(0,2,2,(void*)data);
+    proceder_asignacion_fija(0,3,1,(void*)data);
+    proceder_asignacion_fija(0,4,1,(void*)data);
+    proceder_asignacion_fija(0,1,4,(void*)data); */
+
+    mem_hexdump(list_get(areas_de_swap,0),4096);
+    mem_hexdump(list_get(areas_de_swap,1),4096);
+
+    mostrar_tabla_swap(tablas_de_frames_swap);
+
 
     liberar_conexion(&swamp_server);
     cerrar_programa();
